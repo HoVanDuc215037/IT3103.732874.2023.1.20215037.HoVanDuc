59220cd7481113422b358a6353ce535de258cc8a
Ho Van Duc <hovamduc2003bg@gmail.com>
2023-10-28 17:25:03 +0700

    28/10/2023

---
+++ Lab02/AimsProject/src/blank_package/Cart.java
@@ -0,0 +1,7 @@
+package blank_package;
+
+public class Cart {
+	public static final int  MAX_NUMBERS_ORDERED = 20;
+	private DigitalVideoDisc item0rdered [] = 
+			new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+}
